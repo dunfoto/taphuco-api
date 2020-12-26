e10446c797067a7cbc9fe0054627fd84355261c1
bentrinh <dungtm112@outlook.com>
2020-12-26 15:38:59 +0700

    ALLOW CORS

--- app.js
+++ app.js
@@ -53,10 +53,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 require('./routes')(app)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 
     let err = new Error('Not Found')
     err.status = 404
